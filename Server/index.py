94fca6bba045a4d9af06a4090e9e1bcf986f8259
bonnardot nico <nicobonnardot3@gmail.com>
2022-04-07 19:39:36 +0200

    added Sentiement routes

--- Server/index.py
+++ Server/index.py
@@ -3,3 +3,10 @@ from fastapi import FastAPI
 app = FastAPI()
 
 
+@app.get('/sentiment/c1')
+async def getSentimentC1():
+    return { 'message' : 'test' }
+
+@app.get('/sentiment/c2')
+async def getSentimentc2():
+    return { 'message' : 'test2'}
