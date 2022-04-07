cba46aa7d50832bc147a39196d74d27fb68b115c
Bonnardot Matthieu <Bonnardot.Matthieu@bcg.com>
2022-04-07 19:25:39 +0200

    fixed

--- Server/index.py
+++ Server/index.py
@@ -4,9 +4,9 @@ app = FastAPI()
 
 
 @app.get("/news/C1") #Route to the latest news about the candidate 1(= C1)
-async def root():
+async def get_news_candiate_1():
     return "Here are the latest news about the C1"
 
 @app.get("/news/C2") #Route to the latest news about the candidate 2(= C2)
-async def root():
+async def get_news_candiate_2():
     return "Here are the latest news about the  C2"
